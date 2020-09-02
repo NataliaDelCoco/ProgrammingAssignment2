180288f75e7dd16513419bfc5ae0a95a29e18514
NataliaDelCoco <nataliadelcoco@gmail.com>
2020-09-02 16:36:53 -0300

    Programming Assignment 2: Lexical Resolution By Natalia Del Coco

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# makeCacheMatrix = function of setters and getters 
+#   for inverting a matrix. Creates an object of 
+#   makeCacheMatrix type with behaviors of set, 
+#   get, setinv, getinv.
 
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(new_matrix){
+        x <<- new_matrix
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(new_invmatrix) inv <<- new_invmatrix
+    getinv <- function() inv
+    
+    list(set = set, get = get, setinv = setinv, getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
 
+# cacheSolve = input is a makeCacheMatrix.object.
+#   calculates  the inverse of a matrix and caches it
+#   OR retrieves the already calculated inverse
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if (!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data <-x$get()
+    inv <- solve(data,...)
+    x$setinv(inv)
+    inv
 }
